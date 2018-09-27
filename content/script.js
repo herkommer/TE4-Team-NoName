c99b533b859849e601ff0691e04e3089fbcf869a
IsakHuddinge <isak.nywall@edu.huddinge.se>
2018-09-27 15:42:51 +0200

    removed wow script

--- content/script.js
+++
@@ -1,6 +0,0 @@
-setInterval(function(){
-    var paragraph = document.createElement("p");
-    var text = document.createTextNode("wow");
-    paragraph.appendChild(text);
-    document.body.appendChild(paragraph);
-},1000);
